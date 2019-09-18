d6735d75d5e546ab2b822820d055f4c8f0198b1d
Nova Arisma <novaarisma95@gmail.com>
2019-09-18 14:40:22 +0700

    Initial Commit

---
+++ HealthKitActivities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitActivities
+//
+//  Created by Nova Arisma on 9/18/19.
+//  Copyright © 2019 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
